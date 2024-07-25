8f23cab519f33fab604bd3468603930b82c08caa
NongNgui <omezaza329@gmail.com>
2024-07-25 22:04:42 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <h2>{{ count }}</h2>
+    <button @click="checkCount">Click me</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  data() {
+    return {
+      title: 'Click 100 ครั้งเพืื่อเป็นเกย์',
+      count: 0
+    }
+  },
+  methods: {
+    checkCount() {
+      this.count++
+      if (this.count >= 100 && this.count < 10000) {
+        this.title = 'เป็นเกย์แล้ว ลองกดอีก 10,000 ครั้งเพื่อกลับเป็นชาย'
+      }
+      else if (this.count >= 10000) {
+        this.title = 'มึงนี้มันเกย์ตัวจริง'
+      }
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,12 @@
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: About
+    }
+];
